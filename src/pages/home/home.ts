1069bdf958caf10e4c36838e79f93f00f67091ca
KarinGarciaZ <oscarin962011@hotmail.com>
2018-11-08 10:39:45 -0600

    Calculator alredy finished.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,25 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  operation: string = '';
+  result: string = '';
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  addToOperation( num ) {
+    if ( this.isNumber(num) ){
+      this.operation = this.operation + num;
+      let total = eval(this.operation);
+      this.result = total;
+    } 
+    else 
+      this.operation = this.operation + num;
+  }
+
+  isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
   }
 
 }
